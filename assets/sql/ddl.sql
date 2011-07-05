447beec213c1c95725e973dbd9706c085c1a0860
Nishibayashi Takuji <takuji@senchan.jp>
2011-07-06 08:32:46 +0900

    DDLを更新

--- assets/sql/ddl.sql
+++ assets/sql/ddl.sql
@@ -1,7 +1,26 @@
 CREATE TABLE status (
   id INTEGER PRIMARY KEY,
-  uuid VARCHAR(255) NOT NULL,
+  uuid TEXT NOT NULL,
   service INTEGER NOT NULL,
-  
-  UNIQUE service_uuid (uuid, service)
+  expire_at INTEGER NOT NULL,
+  UNIQUE service_uuid (service, uuid),
+  INDEX expire_at (expire_at)
+);
+
+CREATE TABLE image_cache (
+  id INTEGER PRIMARY KEY,
+  url TEXT NOT NULL,
+  expire_at INTEGER NOT NULL,
+  UNIQUE url (url)
+  INDEX expire_at (expire_at)
+);
+
+CREATE TABLE user_cache (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  service INTEGER NOT NULL,
+  image_url NOT NULL,
+  expire_at INTEGER NOT NULL
+  UNIQUE service_name (service, name),
+  INDEX expire_at (expire_at)
 );
