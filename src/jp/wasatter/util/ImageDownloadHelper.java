902e46f1167570181ab6e0f9cc9d4bef9ae39fd7
Nishibayashi Takuji <takuji@senchan.jp>
2011-07-06 08:33:35 +0900

    画像ダウンロードに使うヘルパー

---
+++ src/jp/wasatter/util/ImageDownloadHelper.java
@@ -0,0 +1,19 @@
+package jp.wasatter.util;
+
+import java.util.HashMap;
+
+import android.graphics.Bitmap;
+
+public class ImageDownloadHelper {
+	public static HashMap<String, String> cache;
+	
+	static {
+		cache = new HashMap<String, String>();
+	}
+	
+	public Bitmap getWithCache(String url) {
+		
+		return null;
+	}
+
+}
